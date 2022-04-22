cab73b6ab3b2ad2697555dc0974d4f640d22f061
Akalia <akaliavasan@gmail.com>
2022-04-22 15:09:27 +0530

    first init

---
+++ public/assets1/js/profile-settings.js
@@ -0,0 +1,246 @@
+/*
+Author       : Dreamguys
+Template Name: Doccure - Bootstrap Template
+Version      : 1.0
+*/
+
+(function($) {
+    "use strict";
+	
+	// Pricing Options Show
+	
+	$('#pricing_select input[name="rating_option"]').on('click', function() {
+		if ($(this).val() == 'price_free') {
+			$('#custom_price_cont').hide();
+		}
+		if ($(this).val() == 'custom_price') {
+			$('#custom_price_cont').show();
+		}
+		else {
+		}
+	});
+	
+	// Education Add More
+	
+    $(".education-info").on('click','.trash', function () {
+		$(this).closest('.education-cont').remove();
+		return false;
+    });
+
+    $(".add-education").on('click', function () {
+		
+		var educationcontent = '<div class="row form-row education-cont">' +
+			'<div class="col-12 col-md-10 col-lg-11">' +
+				'<div class="row form-row">' +
+					'<div class="col-12 col-md-6 col-lg-4">' +
+						'<div class="form-group">' +
+							'<label>Degree</label>' +
+							'<input type="text" name="college_degree[]" class="form-control floating @error("college_degree") is-invalid @enderror" required>' +
+						'</div>' +
+					'</div>' +
+					'<div class="col-12 col-md-6 col-lg-4">' +
+						'<div class="form-group">' +
+							'<label>College/Institute</label>' +
+							'<input type="text" name="college_name[]" class="form-control floating @error("college_name") is-invalid @enderror" required>' +
+						'</div>' +
+					'</div>' +
+					'<div class="col-12 col-md-6 col-lg-4">' +
+						'<div class="form-group">' +
+							'<label>Year of Completion</label>' +
+							'<input type="text" name="college_year[]" class="form-control yearpicker  floating @error("college_year") is-invalid @enderror " required>' +
+						'</div>' +
+					'</div>' +
+				'</div>' +
+			'</div>' +
+			'<div class="col-12 col-md-2 col-lg-1"><label class="d-md-block d-sm-none d-none">&nbsp;</label><a href="#" class="btn btn-danger trash"><i class="far fa-trash-alt"></i></a></div>' +
+		'</div>';
+		
+        $(".education-info").append(educationcontent);
+		$('.yearpicker').datetimepicker({
+			format: 'YYYY',
+			icons: {
+				up: "fas fa-chevron-up",
+				down: "fas fa-chevron-down",
+				next: 'fas fa-chevron-right',
+				previous: 'fas fa-chevron-left'
+			}
+		});
+        return false;
+    });
+	
+	// Experience Add More
+	
+    $(".experience-info").on('click','.trash', function () {
+		$(this).closest('.experience-cont').remove();
+		return false;
+    });
+
+    $(".add-experience").on('click', function () {
+		
+		var experiencecontent = '<div class="row form-row experience-cont">' +
+			'<div class="col-12 col-md-10 col-lg-11">' +
+				'<div class="row form-row">' +
+					'<div class="col-12 col-md-6 col-lg-4">' +
+						'<div class="form-group">' +
+							'<label>Hospital Name</label>' +
+							'<input type="text" name="hospital_name[]" required class="form-control floating @error("hospital_name") is-invalid @enderror">' +
+						'</div>' +
+					'</div>' +
+					'<div class="col-12 col-md-6 col-lg-4">' +
+						'<div class="form-group">' +
+							'<label>From</label>' +
+							'<input type="text" name="start_from[]" required class="form-control yearpicker floating @error("start_from") is-invalid @enderror">' +
+						'</div>' +
+					'</div>' +
+					'<div class="col-12 col-md-6 col-lg-4">' +
+						'<div class="form-group">' +
+							'<label>To</label>' +
+							'<input type="text" name="start_to[]" required class="form-control yearpicker floating @error("start_to") is-invalid @enderror">' +
+						'</div>' +
+					'</div>' +
+					'<div class="col-12 col-md-6 col-lg-4">' +
+						'<div class="form-group">' +
+							'<label>Designation</label>' +
+							'<input type="text" name="designation[]" required class="form-control floating @error("designation") is-invalid @enderror">' +
+						'</div>' +
+					'</div>' +
+				'</div>' +
+			'</div>' +
+			'<div class="col-12 col-md-2 col-lg-1"><label class="d-md-block d-sm-none d-none">&nbsp;</label><a href="#" class="btn btn-danger trash"><i class="far fa-trash-alt"></i></a></div>' +
+		'</div>';
+		
+        $(".experience-info").append(experiencecontent);
+		$('.yearpicker').datetimepicker({
+			format: 'YYYY',
+			icons: {
+				up: "fas fa-chevron-up",
+				down: "fas fa-chevron-down",
+				next: 'fas fa-chevron-right',
+				previous: 'fas fa-chevron-left'
+			}
+		});
+        return false;
+    });
+	
+	// Awards Add More
+	
+    $(".awards-info").on('click','.trash', function () {
+		$(this).closest('.awards-cont').remove();
+		return false;
+    });
+
+    $(".add-award").on('click', function () {
+
+        var regcontent = '<div class="row form-row awards-cont">' +
+			'<div class="col-12 col-md-5">' +
+				'<div class="form-group">' +
+					'<label>Awards</label>' +
+					'<input type="text" name="award_name[]" required class="form-control floating @error("award_name") is-invalid @enderror">' +
+				'</div>' +
+			'</div>' +
+			'<div class="col-12 col-md-5">' +
+				'<div class="form-group">' +
+					'<label>Year</label>' +
+					'<input type="text" name="award_year[]" required class="form-control yearpicker floating @error("award_year") is-invalid @enderror">' +
+				'</div>' +
+			'</div>' +
+			'<div class="col-12 col-md-2">' +
+				'<label class="d-md-block d-sm-none d-none">&nbsp;</label>' +
+				'<a href="#" class="btn btn-danger trash"><i class="far fa-trash-alt"></i></a>' +
+			'</div>' +
+		'</div>';
+		
+        $(".awards-info").append(regcontent);
+		$('.yearpicker').datetimepicker({
+			format: 'YYYY',
+			icons: {
+				up: "fas fa-chevron-up",
+				down: "fas fa-chevron-down",
+				next: 'fas fa-chevron-right',
+				previous: 'fas fa-chevron-left'
+			}
+		});
+        return false;
+    });
+	
+	// Membership Add More
+	
+    $(".membership-info").on('click','.trash', function () {
+		$(this).closest('.membership-cont').remove();
+		return false;
+    });
+
+    $(".add-membership").on('click', function () {
+
+        var membershipcontent = '<div class="row form-row membership-cont">' +
+			'<div class="col-12 col-md-10 col-lg-5">' +
+				'<div class="form-group">' +
+					'<label>Memberships</label>' +
+					'<input type="text" required  name="membership[]" class="form-control floating @error("membership") is-invalid @enderror">' +
+				'</div>' +
+			'</div>' +
+			'<div class="col-12 col-md-2 col-lg-2">' +
+				'<label class="d-md-block d-sm-none d-none">&nbsp;</label>' +
+				'<a href="#" class="btn btn-danger trash"><i class="far fa-trash-alt"></i></a>' +
+			'</div>' +
+		'</div>';
+		
+        $(".membership-info").append(membershipcontent);
+        return false;
+    });
+	
+	// Registration Add More
+	
+    $(".registrations-info").on('click','.trash', function () {
+		$(this).closest('.reg-cont').remove();
+		return false;
+    });
+
+    $(".add-reg").on('click', function () {
+
+        var regcontent = '<div class="row form-row reg-cont">' +
+			'<div class="col-12 col-md-5">' +
+				'<div class="form-group">' +
+					'<label>Registrations</label>' +
+					'<input type="text" name="registration_name[]" required class="form-control floating @error("registration_name") is-invalid @enderror">' +
+				'</div>' +
+			'</div>' +
+			'<div class="col-12 col-md-5">' +
+				'<div class="form-group">' +
+					'<label>Year</label>' +
+					'<input type="text"  name="registration_year[]" required  class="form-control yearpicker floating @error("registration_year") is-invalid @enderror">' +
+				'</div>' +
+			'</div>' +
+			'<div class="col-12 col-md-2">' +
+				'<label class="d-md-block d-sm-none d-none">&nbsp;</label>' +
+				'<a href="#" class="btn btn-danger trash"><i class="far fa-trash-alt"></i></a>' +
+			'</div>' +
+		'</div>';
+        $(".registrations-info").append(regcontent);
+			$('.yearpicker').datetimepicker({
+				format: 'YYYY',
+				icons: {
+					up: "fas fa-chevron-up",
+					down: "fas fa-chevron-down",
+					next: 'fas fa-chevron-right',
+					previous: 'fas fa-chevron-left'
+				}
+			});
+        return false;
+    });
+
+	$("#clinic_images").on("change", function() {
+		if ($("#clinic_images")[0].files.length > 5) {
+			$("#clinic_images").val("");
+			alert("You can select only 5 images");
+		}else{
+			for(var i=0;i<$("#clinic_images")[0].files.length;i++){
+				$('#imgdiv').append(
+					`<div class="upload-images">
+					<img src="${URL.createObjectURL(event.target.files[i])}" alt="Upload Image">
+				</div>`);
+			}
+		}
+	});
+	
+})(jQuery);
